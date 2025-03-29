3fbc60f2748efdb743bf2e897329d204532bf67b
Ragaz79 <gabriel.oragazzi@gmail.com>
2025-03-28 21:41:27 -0300

    jkgjhfjh

---
+++ AgendaContatos/Models/Contato.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaContatos.Models
+{
+    public class Contato
+    {
+        [Key]
+        public int id { get; set; }
+        [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo Numero do Telefone é obrigatório")]
+        public string Numero { get; set; }
+
+        [Required(ErrorMessage = "Selecione uma categoria")]
+        public string CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
