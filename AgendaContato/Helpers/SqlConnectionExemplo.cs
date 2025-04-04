a0ef8cab2c2dc21068a0241f5d83ab3eb6234d9a
Gabriel Toscano <bielango0oficial@gmail.com>
2025-04-04 20:46:46 -0300

    helper de conexao com banco de dados adicionado, .json nao sera mais usado para fazer a conexao

---
+++ AgendaContato/Helpers/SqlConnectionExemplo.cs
@@ -0,0 +1,13 @@
+﻿namespace AgendaContatos.Helpers
+{
+    public static class SqlConnectionExemplo
+    {
+        static string nomeDesktop = "nomeDoComputador";
+
+        static string nomeBancoDados = "nomeDoBanco";
+
+        public static string connectionString = $"Data Source={nomeDesktop}\\SQLEXPRESS;" +
+            $"Initial Catalog={nomeBancoDados};" +
+            "Integrated Security=True;Pooling=False;TrustServerCertificate = true;";
+    }
+}
