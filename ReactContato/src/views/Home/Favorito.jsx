463274163e29effc4b5d43890883ab2f080cc610
Gabriel Toscano <bielango0oficial@gmail.com>
2025-06-16 15:31:19 -0300

    adicionando views ao react, e adicionado controllers de api desenvolvidos

---
+++ ReactContato/src/views/Home/Favorito.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+function favorito() {
+    return (
+        <div>
+            conteudo
+        </div>
+    )
+}
--- ReactContato/src/views/Home/Index.jsx
+++ ReactContato/src/views/Home/Index.jsx
@@ -3,7 +3,42 @@ import { useState } from 'react'
 function index() {
     return (
         <div>
-            conteudo
+            <h1>Contatos</h1>
+            <a href="CriarContato"></a>
+            <br />
+
+            <table className='table table-bordered table-striped mt-3'>
+                <thead className='thead-dark'>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Telefone</th>
+                        <th>Categoria</th>
+                        <th>Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* foreach(){ */}
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                <button>
+                                    <i></i>
+                                </button>
+                            </td>
+                            <td>
+                                <a href=""></a>
+                                <a href=""></a>
+                            </td>
+                        </tr>
+                    {/* } */}
+                </tbody>
+            </table>
+
+            <div className="d-flex justify-content-center mt-4">
+                
+            </div>
         </div>
     )
 }
