5aabf0cfed5ed9047a97b371616d950011522b1f
Gabriel Toscano <bielango0oficial@gmail.com>
2025-06-15 23:58:38 -0300

    criadas alguns arquivos para telas, criar contato e layout.(App.jsx e vies/Home/CriarContato.jsx)

--- ReactContato/src/App.jsx
+++ ReactContato/src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    var res = funcaoController().then(res =>console.log(res));
+  },
+  []);
 
   return (
-    <>
+    <div>
+      <nav className='sidebar'>
+      <h3>Agenda de Contatos</h3>
+      <a href="Home">Contatos</a>
+      <a href="TipoContato">Favoritos</a>
+      </nav>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <main>
+            {/* RENDERIZAR PAGINAS */}
+        </main>
+        <footer className="border-top footer text-muted mt-auto">
+          <div className="container"></div>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ ReactContato/src/views/Home/Index.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+function index() {
+    return (
+        <div>
+            conteudo
+        </div>
+    )
+}
