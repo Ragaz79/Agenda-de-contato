1635015923c36d75836b4c0b9b1b2a5f1dd43bfa
Gabriel_Gomes <=>
2025-06-16 15:38:11 -0300

    teste

--- AgendaContato/obj/Debug/net9.0/AgendaContato.AssemblyInfo.cs
+++ AgendaContato/obj/Debug/net9.0/AgendaContato.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaContato")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c055787a99d47286b0a4b3fbbe961d865d4a50c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2b074294a51b5ed1f151b9c325742ef037a759")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaContato")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaContato")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
